8ed77bdd1c24798f01ba18f90cb34c3435b49a09
Viktrii57 <benvictor.gecure57@gmail.com>
2026-08-23 03:23:57 +0300

    chore: set up Tailwind v4 with placeholder design tokens and deleted tailwind.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"; 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], test: { environment: "jsdom", globals: true, },
+  plugins: [react(), tailwindcss()], test: { environment: "jsdom", globals: true, },
 })
