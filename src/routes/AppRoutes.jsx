9e5ac2d813878b666f85ef83aa12f1c03ccbf116
celestine-021 <wangaricelestine036@gmail.comcla>
2026-08-25 11:11:53 +0300

    feat: farmer dashboard add/edit animal form

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,22 +6,23 @@ import RegisterPage from "../pages/auth/RegisterPage";
 import FarmerDashboard from "../pages/farmer/FarmerDashboard";
 import OrderHistoryPage from "../pages/buyer/OrderHistoryPage";
 import CartPage from "../pages/buyer/CartPage";
-import AnimalDetailPage from "../pages/buyer/AnimalDetailPage"; // ADD THIS
-import CheckoutPage from "../pages/buyer/CheckoutPage"; // 1. ADD THIS
-import OrderConfirmationPage from "../pages/buyer/OrderConfirmationPage"; // 2. ADD THIS
+import AnimalDetailPage from "../pages/buyer/AnimalDetailPage";
+import CheckoutPage from "../pages/buyer/CheckoutPage";
+import OrderConfirmationPage from "../pages/buyer/OrderConfirmationPage";
 import ProtectedRoute from "../features/auth/ProtectedRoute";
+import AnimalFormPage from "../pages/farmer/AnimalFormPage"; 
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/browse" element={<BrowsePage />} />
-      <Route path="/animal/:id" element={<AnimalDetailPage />} /> {/* ADD THIS */}
+      <Route path="/animal/:id" element={<AnimalDetailPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/cart" element={<CartPage />} />
 
-      {/* 3. ADD THESE 2 NEW ROUTES - Only logged in users */}
+      {/* Buyer + Both - Lazima wakuwe logged in */}
       <Route element={<ProtectedRoute />}>
         <Route path="/orders" element={<OrderHistoryPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
@@ -30,8 +31,10 @@ export default function AppRoutes() {
 
       {/* Only farmers */}
       <Route element={<ProtectedRoute roles={['farmer']} />}>
-        <Route path="/dashboard" element={<FarmerDashboard />} />
+        <Route path="/farmer/dashboard" element={<FarmerDashboard />} /> {/* FIXED: nikaweka / */}
         <Route path="/farmer/orders" element={<OrderHistoryPage />} />
+        <Route path="/farmer/add-animal" element={<AnimalFormPage />} /> {/* ADD HII */}
+        <Route path="/farmer/edit-animal/:id" element={<AnimalFormPage />} /> {/* ADD HII */}
       </Route>
     </Routes>
   );
