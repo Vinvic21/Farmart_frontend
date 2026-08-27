fc97f1dd8f12e9bec67cf936033d0821ba124404
Viktrii57 <benvictor.gecure57@gmail.com>
2026-08-27 18:13:01 +0300

    fix: register missing orders reducer in store

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
 import cartReducer from '../features/cart/cartSlice'
 import animalsReducer from '../features/animals/animalsSlice';
+import ordersReducer from '../features/orders/ordersSlice';
 
 export const store = configureStore({
   reducer: {
     animals: animalsReducer,
     auth: authReducer,
     cart: cartReducer,
+    orders: ordersReducer,
   },
 })
 
