6e9a11689538abf76e2990e2d442ae7f193dcea3
Vinvic21 <victorkipngeno877@gmail.com>
2026-08-31 21:43:33 +0300

    Merge develop into master, resolve conflicts in .env, App.jsx, store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
 import cartReducer from '../features/cart/cartSlice'
-<<<<<<< HEAD
 import animalsReducer from '../features/animals/animalsSlice'
 import ordersReducer from '../features/orders/ordersSlice'
 import adminReducer from '../features/admin/adminSlice'
-=======
-import animalsReducer from '../features/animals/animalsSlice';
-import ordersReducer from '../features/orders/ordersSlice';
->>>>>>> develop
 
 export const store = configureStore({
   reducer: {
